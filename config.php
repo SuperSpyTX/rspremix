8cf577ccd72d3ad7efe27754e229a72801c728d5
R29JK10 <R29JK10@b5cae078-e132-11de-9129-6395494df245>
2009-12-05 23:01:49 +0000

    [FIXED] Fixed issue #3 "Major issue with DB information for accountcheck.php" for config.php

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 $INFO['sql_driver']     = 'mysql';
-//DB Configuration is done in DB.php
+$INFO['sql_database']   = "";
+$INFO['sql_user'] = "";
+$INFO['sql_pass'] = "";
+$INFO['sql_host'] = "";
 $INFO['base_url']       = 'http://www.site.com/index.php'; //The link to this script
 $INFO['premium_act']    = '1'; //You should change this number according to the account id number found it the database.
 $INFO['serverload']     = '50'; //Disable download when server load equal this value.
