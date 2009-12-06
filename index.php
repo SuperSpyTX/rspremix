c8b44a318b18033a2101ecfd3b2745fd57de019d
R29JK10 <R29JK10@b5cae078-e132-11de-9129-6395494df245>
2009-12-06 02:13:26 +0000

    [UPDATE] Fixed issue #4 and now invalidated for this update. Learn more at Revision13Update WikiPage.

--- index.php
+++ index.php
@@ -2,46 +2,38 @@
 error_reporting(E_ERROR | E_PARSE);
 define( 'IBIET', 1 );
 @require "config.php";
-@require "function.php";
-//@require "accountcheck.php"; Doesn't need it urgently
 @require "db.php";
 
-class Rapids {
-
     //-----------------------------------------------------
-    // initialize
+    // Initialize
     //-----------------------------------------------------
     function initialize()
     {
-        global $ibiet, $std, $DB, $INFO;
+        global  $DB, $INFO;
 
-        $this->vars = &$INFO;
+        //$this->vars = &$INFO;
         $act = $_GET['action'];
         if($_POST['url'] != "")
         {
-            $act = "makeurl";
+            makeurl($_POST['url']);
         }
-        $action = array("makeurl", "download");
-        if(in_array($act, $action))
-        {
-            $this->$act();
-            return;
+        if($act == "download") {
+        		download();
         }
-        else
-        {
-            $this->welcome();
+        if($act == "" && !$act == "makeurl" && !$act == "download") {
+        welcome();
         }
     }
 
     //-----------------------------------------------------
-    // Welcome Screen
+    // Welcome
     //----------------------------------------------------- 
     function welcome()
     {
-        global $ibiet, $std, $skin, $DB;
+        global  $skin, $DB;
 
         $data = array();
-        $check_serv = $this->serverload();
+        //$check_serv = serverload();
         if($check_serv['overloaded'])
         {
             $stage = "disabled";
@@ -51,7 +43,7 @@ class Rapids {
         {
             if(file_exists("accountcheck.php"))
             {
-                $t = $DB->query("SELECT * FROM accounts_rs WHERE bw > 0 LIMIT 1");
+                $t = $DB->query("SELECT * FROM accounts_rs WHERE bw > 0");
                 $r = $DB->fetch_array($t);
 				$rapidshare = $r['rapidshare'];
 				$bw = $r['bw'];
@@ -72,14 +64,14 @@ class Rapids {
                 $txtstring = "Sorry, there is no more premium accounts available. Please come visit again soon.";
             }
         }
-        if($ibiet->vars['dl_limit_perday'] > 0)
+        if($INFO['dl_limit_perday'] > 0)
         {
             $l_limit = time() - 60*60*24;
             $ip = $_SERVER['REMOTE_ADDR'];
             $l = $DB->query("SELECT * FROM logs WHERE fdate > '$l_limit' AND ip = '$ip'");
             $link_downloaded = $DB->get_num_rows($l);
             $link_downloaded = ($link_downloaded) ? $link_downloaded : 0;
-            $stat = "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'>So far you have downloaded $link_downloaded/{$ibiet->vars['dl_limit_perday']} links limit.<br>Current Rapidshare Bandwidth Available: $bw</span>";
+            $stat = "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'>So far you have downloaded $link_downloaded/{$INFO['dl_limit_perday']} links limit.<br>Current Rapidshare Bandwidth Available: $bw</span>";
         }
 
         $data['html'] .= "<script>\n";
@@ -96,28 +88,100 @@ class Rapids {
         $data['html'] .= "<form action=\"\" method=\"post\" onsubmit=\"return CheckForm()\">\n";
         $data['html'] .= "<input type=\"text\" id=\"url\" style=\"text-align:center\" name=\"url\" value=\"$txtstring\" size=\"60\" onfocus=\"if(this.value=='Enter rapidshare.com URL here'){this.value=''}\" $stage>\n";
         $data['html'] .= "<br><input type=\"submit\" value=\"Download\" $stage></form>";
-        $data['html'] .= $stat;
+        $data['html'] .= "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'>So far you have downloaded $link_downloaded/{$INFO['dl_limit_perday']} links limit.<br>Current Rapidshare Bandwidth Available: $bw</span>";
 
-        $data['title'] = "MegzLeech ~ Rapidshare Premium Link Generator ~";
-        $std->_print($data);
+        $data['title'] = "RsPremiX ~ Rapidshare Premium Link Generator ~";
+        _print($data);
     }
+    // GetFileInfo and Download is now here.
+function getFileInfo($link)
+	{
+		global  $DB;
+		  $t = $DB->query("SELECT * FROM accounts_rs WHERE bw > 0 LIMIT 1");
+                $r = $DB->fetch_array($t);
+				$rapidshare = $r['rapidshare'];
+                list($rlogin, $rpass) = split(":", $rapidshare); 
+
+        $url = parse_url($link);
+        require_once("curl.php");
+        $cook43 = get_cookie($rlogin, $rpass);
+        $vars = "dl.start=PREMIUM&uri={$url['path']}&directstart=1";
+        $head = "Host: {$url['host']}\r\n";
+        $head .= "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n";
+        $head .= "Cookie: $cook43\r\n";
+        $head .= "Content-Type: application/x-www-form-urlencoded\r\n";
+        $head .= "Content-Length: ".strlen($vars)."\r\n";
+        $head .= "Connection: close\r\n\r\n";
+        $fp = @fsockopen($url['host'], 80, $errno, $errstr);
+        if($errstr == "php_network_getaddresses: getaddrinfo failed: name or service not known") {
+        		echo "There is a problem with the Rapidshare URL. Please go check it out.";
+        		exit;
+        }
+        if (!$fp)
+        {
+            echo "The script says <b>$errstr</b>, please try again later.";
+            exit;
+        }
+        fputs($fp, "POST {$url['path']}  HTTP/1.1\r\n");
+        fputs($fp, $head.$vars);
+       
+        $buff = 64;
+        while (!feof($fp))
+        {
+            $tmp .= fgets($fp, $buff);
+            $d = explode("\r\n\r\n", $tmp);
+            if($d[1])
+            {
+                preg_match("#filename=(.+?)\n#", $tmp, $fname);
+                preg_match("#Content-Length: (.+?)\n#", $tmp, $fsize);
+                $h['filename'] = $fname[1] != "" ? $fname[1] : basename($url['path']);
+                $h['fsize'] = $fsize[1];
+                break;
+            }
+        }
+        @fclose($fp);
+        return $h;
+	}
+function getserver($link)
+	{
+		global $ibiet;
+
+        $url = @parse_url($link);
+        $curl = curl_init();
+curl_setopt($curl, CURLOPT_HEADER, 1);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($curl, CURLOPT_URL, $link);
+$fp = curl_exec($curl);
+$start = "<form action=";
+$end = '" method="post">';
+$startposition = strpos($fp,$start);
+if($startposition > 0) {
+$endposition = strpos($fp,$end, $startposition);
+}
+$length = $endposition-$startposition;
+$result = substr($fp,$startposition,$length);
+$result = substr($result, 14);
+return $result;
+	}
 
     //-----------------------------------------------------
     // Generate Link Screen
     //-----------------------------------------------------
-    function makeurl()
+    function makeurl($lin)
     {
-        global $ibiet, $std, $DB;
+        global  $DB;
 
         $data = array();
-        $link = addslashes(trim($_POST['url'])) ;
+        $link = addslashes(trim($lin)) ;
         $url = @parse_url($link);
         $ip = $_SERVER['REMOTE_ADDR'];
         if($url['host'] != "rapidshare.com" || !preg_match("#^/files/#", $url['path']))
         {
             $data['html'] = "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>Please enter a valid rapidshare.com link.</b></span>";
             $data['title'] = "Error";
-            $std->_print($data);
+            _print($data);
             return;
         }
         $refererr = @parse_url($_SERVER['HTTP_REFERER']);
@@ -125,35 +189,34 @@ class Rapids {
         {
             $data['html'] = "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>No leeching, visit http://{$_SERVER['HTTP_HOST']} for more info</b></span>";
             $data['title'] = "Error";
-            $std->_print($data);
+            _print($data);
             return;
         }
-        if(!$ibiet->vars['premium_act'])
-        {
-            $data['html'] = "Sorry, there is no premium accounts available. Please come visit again soon.";
-            $data['title'] = "Error";
-            $std->_print($data);
-            return;
-        }
-        if($ibiet->vars['dl_limit_perday'] > 0)
+        if($INFO['dl_limit_perday'] > 0)
         {
             $l_limit = time() - 60*60*24;
             $l = $DB->query("SELECT * FROM logs WHERE fdate > '$l_limit' AND ip = '$ip'");
             $link_downloaded = $DB->get_num_rows($l);
             $link_downloaded = ($link_downloaded) ? $link_downloaded : 0;
-            if($link_downloaded >= intval($ibiet->vars['dl_limit_perday']))
+            if($link_downloaded >= intval($INFO['dl_limit_perday']))
             {
-                $data['html'] .= "You have downloaded $link_downloaded/{$ibiet->vars['dl_limit_perday']} links already.";
+                $data['html'] .= "You have downloaded $link_downloaded/{$INFO['dl_limit_perday']} links already.";
 
                 $data['title'] = "Error";
-                $std->_print($data);
+                _print($data);
             }
         }
         $t = $DB->query("SELECT * FROM accounts_rs WHERE bw > 0 LIMIT 1");
         $r = $DB->fetch_array($t);
-
-        $full_link = $std->getserver($link);
-        $try = $std->getFileInfo($full_link, $r);
+        $rapidshare = $r['rapidshare'];
+        if($rapidshare == "") {
+        		$data['html'] = "Sorry, there is no premium accounts available. Please come visit again soon.";
+            $data['title'] = "Error";
+            _print($data);
+            return;
+        }
+        $full_link = getserver($link);
+        $try = getFileInfo($full_link, $r);
         $filename = $try['filename'];
         $fsize = $try['fsize'];
 
@@ -166,7 +229,7 @@ class Rapids {
         $data['html'] .= "<br><a href='$dlurl'><span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'>Click here to download this file <br><b>$filename</b></span></a>\n";
 
         $data['title'] = "Download";
-        $std->_print($data);
+        _print($data);
     }
 
     //-----------------------------------------------------
@@ -174,39 +237,31 @@ class Rapids {
     //-----------------------------------------------------
     function download()
     {
-		global $ibiet, $std, $DB;
+		global  $DB;
 
         $id = addslashes(trim($_GET['id'])) ;
             $t = $DB->query("SELECT * FROM accounts_rs WHERE bw > 0 LIMIT 1");
        $r = $DB->fetch_array($t);
 	     $rapidshare = $r['rapidshare'];
        list($rlogin, $rpass) = split(":", $rapidshare); 
-        $limit = time() - 60*60*1; //Only download link that was generated in the last 6 hours
+        $limit = time() - 60*60*1; //Only download link that was generated in the last hour.
         $ip = $_SERVER['REMOTE_ADDR'];
-        $q = $DB->query("SELECT * FROM logs WHERE fid=$id");
+        $q = $DB->query("SELECT * FROM logs WHERE fid=$id AND ip=$ip");
         $row = $DB->fetch_array($q);
         $qcheck = $row['valid'];
         $url = @parse_url($row['furl']);
         if($row['furl'] == "")
         {
             @header("HTTP/1.0 404 Not Found");
-            echo "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>404! Invalid Link</b></span>";
+            echo "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>Invalid or Broken Link.</b></span>";
             exit;
         }
                 
         if($qcheck > 0) {
-        		 echo "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>404! Invalid Link</b></span>";
+        		 echo "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>Invalid or Broken Link.</b></span>";
         		 exit;
         }
          $DB->query("UPDATE `logs` SET `valid` = '1' WHERE `fid` = $id;");
-        /*
-        @header("Cache-Control:");
-        @header("Cache-Control: public");
-        @header("Content-Type: application/octet-stream");
-        @header("Content-Disposition: attachment; filename=".$row['filename']);
-        @header("Accept-Ranges: bytes");
-        */
-        // Epic Failure. Below is Epic Fix!
         header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename='.$row['filename']);
@@ -278,7 +333,7 @@ header('Pragma: public');
     //-----------------------------------------------------
     function serverload()
     {
-        global $ibiet, $std, $DB;
+        global  $std, $DB;
 
         if ( @file_exists('/proc/loadavg') )
         {
@@ -288,9 +343,9 @@ header('Pragma: public');
                 @fclose( $fh );
                 $load_avg = explode( " ", $data );
                 $server_load = trim($load_avg[0]);
-                if ($server_load > $ibiet->vars['serverload'])
+                if ($server_load > $INFO['serverload'])
                 {
-                    return array('overloaded' => 1, 'loaded' => $server_load, 'msg' => "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>Sorry, the server is overloaded, please try again in a moment</b></span>");
+                    return array('overloaded' => 1, 'loaded' => $server_load, 'msg' => "Sorry, the server is overloaded, please try again in a moment</b></span>");
                 }
             }
         }
@@ -300,15 +355,35 @@ header('Pragma: public');
             {
                 preg_match( "/(?:averages)?\: ([0-9\.]+),[\s]+([0-9\.]+),[\s]+([0-9\.]+)/", $serverstats, $load );
                 $server_load = $load[1];
-                if ($server_load > $ibiet->vars['serverload'])
+                if ($server_load > $INFO['serverload'])
                 {
-                    return array('overloaded' => 1, 'loaded' => $server_load, 'msg' => "<span style='color:#688000; background-color:#BBDB54; font-size : 10pt; text-decoration: none; font-family: Trebuchet MS;'><b>Sorry, the server is overloaded, please try again in a moment</b></span>");
+                    return array('overloaded' => 1, 'loaded' => $server_load, 'msg' => $server_load);
                 }
             }
         }
     }
-} // End Class
+     function _print($data)
+    {
+        global  $DB;
 
-$ibiet = new Rapids;
-$ibiet->initialize();
+        $t = "<html>\n";
+        $t .= "<head>\n";
+        $t .= "<title>{$data['title']}</title>\n";
+        $t .= "<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\" />\n";
+        $t .= "</head>\n\n";
+        $t .= "<body>\n";
+        $t .= "<table width='100%' height='100%'><tr><td align='center' valign='top'>\n";
+        $t .= $data['html'];
+        $t .= "\n</td></tr></table>\n";
+        $t .= "</body>\n";
+        $t .= "</html>";
+        $DB->close();
+		@header("Cache-Control: no-cache, must-revalidate, max-age=0");
+		@header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		@header("Pragma: no-cache");
+        print $t;
+        exit;
+    }
+
+    initialize();
 ?>
---
+++ test.php
@@ -0,0 +1,75 @@
+<?php
+if(isset($_GET['step1'])) {
+	?>
+	The fsockopen() test. This test will check if fsockopen is working properly.. Is it a epic? or a fail?<br>
+	<?php
+	$fp = fsockopen("www.rapidshare.com", 80, $errno, $errstr, 30);
+if (!$fp) {
+    echo "<font size='10'>FAIL!</font><br>fsockopen() is not working! it returned $errstr while it loaded. Please check your installation of the fsockopen() function. Also make sure that allow_fopen_url is enabled on php.ini or this script will fail.<br>Debug:";
+    print($errstr);
+    exit();
+} else {
+    $out = "GET / HTTP/1.1\r\n";
+    $out .= "Host: www.example.com\r\n";
+    $out .= "Connection: Close\r\n\r\n";
+    fwrite($fp, $out);
+    while (!feof($fp)) {
+        fgets($fp, 128);
+    }
+    fclose($fp);
+   }
+?>
+<br><font size=5>Epic Success!</font><br><br>fsockopen() data Successfully retrieved information from rapidshare.com Click <a href="test.php?step2">Here</a> to continue testing cURL!
+<?php
+exit();}
+
+if(isset($_GET['step2'])) {
+	?>
+	The cURL Test. This test will check if cURL is working properly.. Is it a epic? or a fail?
+	<?php
+	$curl = curl_init();
+//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_NOBODY, 1);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "login=".$User."&password=".$Pass);
+//curl_setopt($curl, CURLOPT_COOKIEJAR, '/tmp/omfg342.txt');
+//curl_setopt($curl, CURLOPT_COOKIEFILE, '/tmp/omfg342.txt');
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($curl, CURLOPT_URL, "http://rapidshare.com");
+$page = curl_exec($curl);
+if(curl_error($curl)) {
+	print "<font size='10'>FAILURE!</font><br>cURL is not working! it returned a error while it loaded. Please check your installation of cURL is correct. Also make sure you recompiled PHP while you were at it.<br><br>Debug: ";
+	print(curl_error($curl));
+	exit();
+}
+//echo($page);
+?>
+<br><font size=5>Epic Success!</font><br><br>cURL data Successfully retrieved information from rapidshare.com Click <a href="test.php?step3">Here</a> to continue testing cURL! but with SSL! This is the final test! Is it a epic success? or a epic failure?
+<?php
+exit(); }
+if(isset($_GET['step3'])) {
+		$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_NOBODY, 1);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, false);
+curl_setopt($curl, CURLOPT_POSTFIELDS, "login=".$User."&password=".$Pass);
+//curl_setopt($curl, CURLOPT_COOKIEJAR, '/tmp/omfg342.txt');
+//curl_setopt($curl, CURLOPT_COOKIEFILE, '/tmp/omfg342.txt');
+curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($curl, CURLOPT_URL, "https://ssl.rapidshare.com/cgi-bin/premiumzone.cgi");
+$page = curl_exec($curl);
+if(curl_error($curl)) {
+	print "<font size='10'>EPIC FAIL!</font><br>cURL with SSL is not working! it returned a error while it loaded. Please check your installation of cURL SSL is correct. Also make sure you recompiled PHP while you were at it. Sadly now you won't be able to use this generator without it.<br><br>Debug: ";
+	print(curl_error($curl));
+	exit();
+}
+?>
+<font size=10>EPIC SUCCESS!</font><br><br>DONE! YAY! cURL data Successfully retrieved information from rapidshare.com! Congratulations! You are finished! You can now run RsPremiX with no problem. Don't forget to keep this if you make changes to your PHP install.
+<?php exit(); } ?>
+Welcome to the test. This test will prove that RsPremiX will work fine. if you fail at any of these tests. You will NEVER be able to use RsPremiX. and this goes to you. pankajabcd! You better not fail this test! This has also been checked for validity so there wont be any runtime PHP errors. Not unless you are blind. Oh please..<br><br>Click <a href="test.php?step1">Here</a> to begin testing. Hope you don't fail.
+
+	
